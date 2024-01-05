87c2f059f27ba3e1c9ac20a733a5f5e32e8cb3ce
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2024-01-05 13:29:13 -0800

    Update Posts.js

--- frontend/src/pages/Posts.js
+++ frontend/src/pages/Posts.js
@@ -41,7 +41,7 @@ export default function Posts() {
         <div className='animate'>
             <h1 className='header'>Home</h1>
             <p className='postdesc'>Welcome to CosmicOdyssey! CosmicOdyssey is a blog where fans of astronomy can come together and post their astronomical observations. Users can give each post an upvote (supernova) and a downvote (blackhole). You can view the post with the most supernova votes and the post with the most black hole votes via the links above.</p>
-            <motion.button style={{opacity: ctx.isLoggedIn ? 1 : 0}} id='newpost' onClick={handleRedirect} whileHover={{scale: 1.1, border: '3px outset navy'}}>New Post</motion.button>
+            <motion.button style={{opacity: ctx.isLoggedIn ? 1 : 0, visibility: ctx.isLoggedIn ? 'visible' : 'hidden'}} id='newpost' onClick={handleRedirect} whileHover={{scale: 1.1, border: '3px outset navy'}}>New Post</motion.button>
             {isLoading && <Loading/>}
             {error && <h2 className='invalid center'>{error}</h2>}
             {!isLoading && !error && posts.length === 0 && <h2 className='center'>No posts.</h2>}
